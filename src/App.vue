5611f228441841c37a4e852365861770dd3911e9
Rumi <tzuruc@gmail.com>
2025-08-12 23:29:50 +0800

    開新分頁，現在要去除了

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>helol</div>
+  <div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/todolist">todolist</RouterLink>
+    </nav>
+    <RouterView />
+  </div>
 </template>
 
 <script setup></script>
